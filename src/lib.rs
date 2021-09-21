b8250c528c25a61502c98084c1dc6eaa9d95db09
ivanovvasil99 <ivanovvasil99@gmail.com>
2021-09-21 20:26:32 +0300

    Starting transfer and test writing

--- src/lib.rs
+++ src/lib.rs
@@ -21,8 +21,28 @@ mod tests {
     #[test]
     fn length_calculator() {
         let q = array![0.0,1.0,0.0];
-        assert!(quaternion_length(q.view()) - 1 <= 0.00000000001)
+        assert!(quaternion_length(q.view()) - 1.0 <= 0.00000000001)
     }
+    #[test]
+    fn cross() {
+        let q = array![1.0, 2.0, 3.0];
+        let u = array![1.0, 5.0, 12.0];
+        let c = cross_product(q.view(), u.view());
+        let dif = c - array![9.0, -9.0, 3.0];
+        assert!(dif.dot(&dif).abs() <=0.00001);
+    }
+
+    #[test]
+    fn rotation() {
+        let v = array![2.0, 3.0,5.0];
+        let q = array![0.25,0.25,0.0,0.0];
+        let q = normalize_quaternion(q.view());
+        let expected = array![2.9999999999999996, 2.0, -4.999999999999999];
+        let dif = rotate_vec_by_quat(v.view(), q.view())-expected;
+        let len_dif = dif.dot(&dif).abs();
+        assert!(len_dif <= 0.0000000001);
+    }
+
 }
 
 
@@ -37,9 +57,21 @@ fn cross_product(a: VecfView, b: VecfView) -> Vecf {
     (a[0]*b[1]-a[1]*b[0])]
 }
 
+fn normalize_quaternion(q: VecfView) -> Vecf {
+    q.to_owned() / (q.dot(&q)).sqrt()
+}
+
+fn normalize_qd(qd: VecfView) -> Vecf {
+    let q = qd.slice(s![..4]);
+    let d = qd.slice(s![4..]);
+    let q = normalize_quaternion(q);
+    array![q[0], q[1], q[2], q[3], d[0], d[1], d[2]]
+}
+
 fn rotate_vec_by_quat(v: VecfView, q: VecfView) -> Vecf {
     let u = array![q[0], q[1], q[2]];
     let u = u.view();
     let s = q[3];
     2.0 * u.dot(&v) * &u + (s * s - u.dot(&u)) * &v + 2.0 * s * cross_product(u, v)
 }
+
